d1747e2ef2b58b8dbab0da7dad8c356130ccf67a
Alenki <omarhack810@gmail.com>
2024-12-24 19:53:55 +0300

    Added functional back to top button. (not fully styled yet)

--- script.js
+++ script.js
@@ -11,3 +11,30 @@ menuBtn.addEventListener('click', () => {
         menuOpen = false;
     }
 });
+// End of Navbar Menu Button
+
+// Back to top button
+//Get the button
+let toTop_button = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        toTop_button.style.display = "block";
+    } 
+    else {
+        toTop_button.style.display = "none";
+    }
+}
+// When the user clicks on the button, scroll to the top of the document
+toTop_button.addEventListener("click", backToTop);
+
+function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+//End of Back to top button
