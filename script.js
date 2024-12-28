fc8ada199d9a81dd165f92f31c76a611bc0db983
Alenki <omarhack810@gmail.com>
2024-12-28 14:28:37 +0300

    Добавил функционал кнопки режима работы

--- script.js
+++ script.js
@@ -38,3 +38,25 @@ function backToTop() {
     document.documentElement.scrollTop = 0;
 }
 //End of Back to top button
+
+// 2 main buttons on top
+var worktimes_btn = document.getElementById("worktimesUnlock");
+var worktimes_modal = document.getElementById("worktimesDiv");
+var worktimes_isopen = false;
+document.onclick = function(e) {
+    if ((e.target.id !== 'worktimesDiv') && ((e.target.id !== 'worktimesUnlock')) && (worktimes_isopen===true)) {
+        worktimes_modal.style.display = "none";
+        worktimes_isopen = false;
+    }
+};
+worktimes_btn.onclick = function() {
+    if (worktimes_isopen === false) {
+        worktimes_modal.style.display = "block";
+        worktimes_isopen = true;
+    } else {
+        worktimes_modal.style.display = "none";
+        worktimes_isopen = false;
+    }
+};
+
+// End of 2 main buttons on top
