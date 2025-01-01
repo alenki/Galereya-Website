3563bace9399765a9599e98c29793d5f04e64bd4
Alenki <omarhack810@gmail.com>
2025-01-01 20:41:02 +0300

    Copied the back-to-top button and navbar menu button js code to the store js code

--- js/store.js
+++ js/store.js
@@ -1,4 +1,44 @@
 
+// Navbar Menu Button
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+    if(!menuOpen) {
+        menuBtn.classList.add('open');
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+    }
+});
+// End of Navbar Menu Button
+
+// Back to top button
+//Get the button
+let toTop_button = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        toTop_button.style.display = "block";
+    } 
+    else {
+        toTop_button.style.display = "none";
+    }
+}
+// When the user clicks on the button, scroll to the top of the document
+toTop_button.addEventListener("click", backToTop);
+
+function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+//End of Back to top button
+
 // Categories
 const categoryHeading = document.getElementById("categoryHeading");
 var category = localStorage.getItem("category")
