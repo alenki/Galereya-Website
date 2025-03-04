a119766a5c01438b23064fe6bbc9653ec6748d7e
Alenki <omarhack810@gmail.com>
2025-03-04 23:48:02 +0300

    PC Menu buttons functionality

--- js/script.js
+++ js/script.js
@@ -19,6 +19,59 @@ menuBtn.addEventListener('click', () => {
 });
 // End of Navbar Menu Button
 
+
+// Navbar Buttons
+// Магазины
+document.getElementById("menuButton-1").onclick = function () { 
+    localStorage.setItem("category", "Магазины");
+    window.location.href = "stores.html"; 
+};
+// Еда
+document.getElementById("menuButton-2").onclick = function () { 
+    localStorage.setItem("category", "Еда");
+    window.location.href = "stores.html"; 
+};
+// Развлечения
+document.getElementById("menuButton-3").onclick = function () { 
+    localStorage.setItem("category", "Развлечения");
+    window.location.href = "stores.html"; 
+};
+// Детям
+document.getElementById("menuButton-4").onclick = function () { 
+    localStorage.setItem("category", "Детям");
+    window.location.href = "stores.html"; 
+};
+// Контакты
+document.getElementById("menuButton-5").onclick = function () { 
+    var modal = new bootstrap.Modal(document.getElementById('contactsModal'), {
+        keyboard: false
+    })
+    modal.toggle()
+};
+// Контакты
+document.getElementById("menuButton-6").onclick = function () { 
+    var modal = new bootstrap.Modal(document.getElementById('feedbackModal'), {
+        keyboard: false
+    })
+    modal.toggle()
+};
+// Контакты
+document.getElementById("menuButton-7").onclick = function () { 
+    var modal = new bootstrap.Modal(document.getElementById('aboutModal'), {
+        keyboard: false
+    })
+    modal.toggle()
+};
+// Контакты
+document.getElementById("menuButton-8").onclick = function () { 
+    var modal = new bootstrap.Modal(document.getElementById('rulesModal'), {
+        keyboard: false
+    })
+    modal.toggle()
+};
+// End of Navbar buttons
+
+
 // Mobile Nav
 /* Set the width of the side navigation to 250px */
 function open_sidenav() {
